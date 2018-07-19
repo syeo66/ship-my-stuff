ad57a0a7ddb19ddb2071bbe3737cced23a35cb78
Red Ochsenbein <red@redochsenbein.ch>
2018-07-19 18:16:58 +0200

    Added a check for the address on input

--- src/lib/components/app_configuration.js
+++ src/lib/components/app_configuration.js
@@ -20,6 +20,10 @@ class AppConfiguration extends Component {
 
     componentDidMount() {
         $('#configurationModal').on('hidden.bs.modal', _ => {
+            if (!this.isAddress(this.state.account)) {
+                this.setState(prevState => ({...prevState, account: ''}));
+            }
+
             Web3Connector.connectionData.account = this.state.account;
             Web3Connector.connectionData.ipfsAddress = this.state.ipfsAddress || 'https://ipfs.infura.io:5001/';
             Web3Connector.connectionData.nodeAddress = this.state.nodeAddress || 'https://localhost:8545/';
@@ -58,6 +62,33 @@ class AppConfiguration extends Component {
 
     handleSubmit(e) {
         e.preventDefault();
+    }
+
+    isAddress(address) {
+        if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
+            // check if it has the basic requirements of an address
+            return false;
+        }
+        if (/^(0x)?[0-9a-f]{40}$/.test(address) || /^(0x)?[0-9A-F]{40}$/.test(address)) {
+            // If it's all small caps or all all caps, return true
+            return true;
+        }
+
+        // Otherwise check each case
+        return this.isChecksumAddress(address);
+    }
+
+    isChecksumAddress(address) {
+        // Check each case
+        address = address.replace('0x','');
+        var addressHash = sha3(address.toLowerCase());
+        for (var i = 0; i < 40; i++ ) {
+            // the nth letter should be uppercase if the nth digit of casemap is 1
+            if ((parseInt(addressHash[i], 16) > 7 && address[i].toUpperCase() !== address[i]) || (parseInt(addressHash[i], 16) <= 7 && address[i].toLowerCase() !== address[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
     render() {
